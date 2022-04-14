9c32fb6c4acca6d41edad8f949d858f06340ce7f
speakless2000 <95895867+speakless2000@users.noreply.github.com>
2022-04-14 17:01:58 +0700

    TuanNA - Update Account Service
    TuanNA - Update Account Service

--- Capstone-Back-End/src/main/java/backend/service/AccountService.java
+++ Capstone-Back-End/src/main/java/backend/service/AccountService.java
@@ -28,60 +28,82 @@ public class AccountService {
     }
 
     public Account getById(int id) {
-        return accountRepo.findById(id).get();
+        if (accountRepo.findById(id).isPresent())
+            return accountRepo.findById(id).get();
+        else
+            return null;
     }
 
-    public boolean checkUsernameExisted(String username){
-        if(accountRepo.getByUsername(username)==null){
+    public boolean checkUsernameExisted(String username) {
+        if (accountRepo.getByUsername(username) == null) {
             return false;
         }
         return true;
     }
 
-    public Account changePassword(int id, String newPassword){
+    public Account changePassword(int id, String newPassword) {
+        if(!accountRepo.findById(id).isPresent()){
+            return null;
+        }
         Account a = accountRepo.findById(id).get();
         a.setPassword(getEncryptedString(newPassword));
         return accountRepo.save(a);
     }
 
     public String getSaveMessage(CreateUpdateAccountRequest request) {
-        if (accountRepo.getByUsername(request.getUsername()) != null) {
-            return "Tên đăng nhập đã được sử dụng";
+        Account newAccount = new Account();
+        newAccount.setId(request.getId());
+        newAccount.setUsername(request.getUsername());
+        newAccount.setPassword(getEncryptedString(request.getPassword()));
+        newAccount.setMaNv(request.getMaNv().toUpperCase());
+        newAccount.setRole(roleRepo.findById(request.getRoleID()).get());
+
+        // Add
+        if (request.getId() == null) {
+            if (accountRepo.getByUsername(request.getUsername()) != null) {
+                return "Tên đăng nhập đã được sử dụng";
+            } else if (accountRepo.getByMaNv(request.getMaNv().toUpperCase()) != null) {
+                return "Mã nhân viên đã được sử dụng";
+            } else if (userRepo.findById(request.getMaNv().toUpperCase()) == null) {
+                return "Chưa có người dùng được tạo với mã nhân viên này";
+            } else {
+                accountRepo.save(newAccount);
+                return null;
+            }
         }
-        else if (accountRepo.getByMaNv(request.getMaNv().toUpperCase()) != null) {
-            return "Mã nhân viên đã được sử dụng";
-        }
-        else if (userRepo.findById(request.getMaNv().toUpperCase()) == null) {
-            return "Chưa có người dùng được tạo với mã nhân viên này";
-        }
+        // Edit
         else {
-            Account newAccount = new Account();
-            newAccount.setId(request.getId());
-            newAccount.setUsername(request.getUsername());
-            newAccount.setPassword(getEncryptedString(request.getPassword()));
-            newAccount.setMaNv(request.getMaNv().toUpperCase());
-            newAccount.setRole(roleRepo.findById(request.getRoleID()).get());
-            accountRepo.save(newAccount);
-            return null;
+            Account oldAccount = accountRepo.findById(request.getId()).get();
+            if (!oldAccount.getUsername().equalsIgnoreCase(newAccount.getUsername()) && accountRepo.getByUsername(request.getUsername()) != null) {
+                return "Tên đăng nhập đã được sử dụng";
+            } else if (!oldAccount.getMaNv().equalsIgnoreCase(newAccount.getMaNv()) && accountRepo.getByMaNv(request.getMaNv().toUpperCase()) != null) {
+                return "Mã nhân viên đã được sử dụng";
+            } else if (!userRepo.findById(request.getMaNv().toUpperCase()).isPresent()) {
+                return "Chưa có người dùng được tạo với mã nhân viên này";
+            } else {
+                accountRepo.save(newAccount);
+                return null;
+            }
         }
+
     }
 
     public List<Account> getAll() {
         return accountRepo.findAll();
     }
 
-    public void delete(int id){
+    public void delete(int id) {
         accountRepo.deleteById(id);
     }
 
-    public Account getByMaNv(String maNv){
+    public Account getByMaNv(String maNv) {
         return accountRepo.getByMaNv(maNv);
     }
 
-    public Account getForgotAccount(String encryptedID){
+    public Account getForgotAccount(String encryptedID) {
         int lastID = accountRepo.getLastID();
-        for(int i=1;i<lastID;i++){
-            if(getEncryptedString(i+"").equals(encryptedID)){
+        for (int i = 1; i < lastID; i++) {
+            if (getEncryptedString(i + "").equals(encryptedID)) {
                 return accountRepo.findById(i).get();
             }
         }
