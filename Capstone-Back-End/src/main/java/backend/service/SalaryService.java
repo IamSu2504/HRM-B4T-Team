54a0617094f776c2b988160ef547f25f62468367
speakless2000 <speakless2000@gmail.com>
2022-04-25 00:29:13 +0700

    TuanNA - 24/4/2022

--- Capstone-Back-End/src/main/java/backend/service/SalaryService.java
+++ Capstone-Back-End/src/main/java/backend/service/SalaryService.java
@@ -2,6 +2,7 @@ package backend.service;
 
 import backend.entity.Salary;
 import backend.repository.SalaryRepository;
+import backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,43 +11,45 @@ import java.util.List;
 @Service
 public class SalaryService {
     @Autowired
-    private SalaryRepository repo;
+    private SalaryRepository salaryRepo;
 
-    public List<Salary> getAll(){
-        return repo.findAll();
+    @Autowired
+    private UserRepository userRepo;
+
+    public List<Salary> getAll() {
+        return salaryRepo.findAll();
     }
 
-    public Salary getById(int id)
-    {
-        if(repo.findById(id).isPresent()){
-            return repo.findById(id).get();
-        }
-        else{
+    public Salary getById(int id) {
+        if (salaryRepo.findById(id).isPresent()) {
+            return salaryRepo.findById(id).get();
+        } else {
             return null;
         }
     }
 
-    public Salary save(Salary newSalary)
-    {
+    public Salary save(Salary newSalary) {
         // update
-        if(newSalary.getId()!=null){
-            Salary oldSalary = repo.findById(newSalary.getId()).get();
-            if((newSalary.getMaHD()) == (oldSalary.getMaHD())){
-                if(repo.getByMaHD(newSalary.getMaHD().getId())==null){
-                    return repo.save(newSalary);
-                }
-                else{
+        if (newSalary.getId() != null) {
+            Salary oldSalary = salaryRepo.findById(newSalary.getId()).get();
+            if ((newSalary.getMaHD()) == (oldSalary.getMaHD())) {
+                if (salaryRepo.getByMaHD(newSalary.getMaHD().getId()) == null) {
+                    return salaryRepo.save(newSalary);
+                } else {
                     return null;
                 }
             }
-            return repo.save(newSalary);
+            double tongLuong = newSalary.getLuongCoBan() + newSalary.getPhuCapKhac() + userRepo.findById(newSalary.getMaHD().getMaNV()).get().getChucVu().getPhuCap();
+            newSalary.setTongLuong(tongLuong);
+            return salaryRepo.save(newSalary);
         }
         // add
-        else{
-            if(repo.getByMaHD(newSalary.getMaHD().getId())==null){
-                return repo.save(newSalary);
-            }
-            else{
+        else {
+            if (salaryRepo.getByMaHD(newSalary.getMaHD().getId()) == null) {
+                double tongLuong = newSalary.getLuongCoBan() + newSalary.getPhuCapKhac() + userRepo.findById(newSalary.getMaHD().getMaNV()).get().getChucVu().getPhuCap();
+                newSalary.setTongLuong(tongLuong);
+                return salaryRepo.save(newSalary);
+            } else {
                 return null;
             }
         }
