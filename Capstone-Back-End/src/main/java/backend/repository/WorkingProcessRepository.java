80f8291418b615282c34e1f64049b1e452f9ee0e
speakless2000 <speakless2000@gmail.com>
2022-06-05 04:15:03 +0700

    TuanNA - 5/6/2022

--- Capstone-Back-End/src/main/java/backend/repository/WorkingProcessRepository.java
+++ Capstone-Back-End/src/main/java/backend/repository/WorkingProcessRepository.java
@@ -1,6 +1,5 @@
 package backend.repository;
 
-import backend.entity.PositionCategory;
 import backend.entity.WorkingProcess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,7 +18,16 @@ public interface WorkingProcessRepository extends JpaRepository<WorkingProcess, 
     @Query(value = "SELECT * FROM hrm_b4t.quatrinhcongtac where ma_nv = ?", nativeQuery = true)
     List<WorkingProcess> getByMaNV(String maNV);
 
+    @Query(value = "SELECT * FROM hrm_b4t.quatrinhcongtac where ma_nv = ? order by ngay_vao desc limit 1", nativeQuery = true)
+    WorkingProcess getLastByEmp(String maNV);
+
     @Query(value = "select w from WorkingProcess w where w.maNV = :maNV and w.ngayVao <= :end and (w.ngayRa >= :start or w.ngayRa is null)")
     WorkingProcess getByEmp(@Param("maNV") String maNV, @Param("start") Date start, @Param("end") Date end);
+
+    @Query(value = "select w from WorkingProcess w where w.maNV = :maNV and ((w.ngayVao between :start and :end) or(w.ngayRa between :start and :end))")
+    List<WorkingProcess> getByEmpInRange(@Param("maNV") String maNV, @Param("start") Date start, @Param("end") Date end);
+
+//    @Query(value = "select * from luongnhanvien where ma_hop_dong = ? and ngay_hieu_luc between ? and ?" , nativeQuery = true)
+//    WorkingProcess getInRange(String maHopDong, String start, String end);
 
 }
--- Capstone-Back-End/src/main/java/backend/service/WorkingProcessService.java
+++ Capstone-Back-End/src/main/java/backend/service/WorkingProcessService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -24,52 +26,80 @@ public class WorkingProcessService {
     @Autowired
     private PositionCategoryRepository positionCategoryRepository;
 
-    public List<WorkingProcess> getAll(){
+    public List<WorkingProcess> getAll() {
         return repo.findAll();
     }
 
-    public WorkingProcess getById(int id)
-    {
-        if(repo.findById(id).isPresent()){
+    public WorkingProcess getById(int id) {
+        if (repo.findById(id).isPresent()) {
             return repo.findById(id).get();
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public List<WorkingProcess> getByMaNV(String maNV){
+    public List<WorkingProcess> getByMaNV(String maNV) {
         return repo.getByMaNV(maNV);
     }
 
-    public String getSaveMessage(CreateUpdateWorkingProcess request)
-    {
+    public String getSaveMessage(CreateUpdateWorkingProcess request) {
         WorkingProcess newWorkingProcess = getNewWorkingProcess(request);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+        Date start;
+        Date end;
+        Calendar c = Calendar.getInstance();
+        c.setTime(newWorkingProcess.getNgayVao());
+        c.set(Calendar.HOUR,0);
+        c.set(Calendar.MINUTE,0);
+        c.set(Calendar.SECOND,0);
+        start = c.getTime();
+
+        if(newWorkingProcess.getNgayRa()==null){
+            c.set(8099,1,1,0,0,0);
+            end = c.getTime();
+        }
+        else{
+            c.setTime(newWorkingProcess.getNgayRa());
+            c.set(Calendar.HOUR,0);
+            c.set(Calendar.MINUTE,0);
+            c.set(Calendar.SECOND,0);
+            end = c.getTime();
+        }
+        if (compareDate(start, end) >= 0) {
+            return "Start date must be before end date";
+        }
         // update
-        if(newWorkingProcess.getId()!=null){
-            WorkingProcess oldWorkingProcess = repo.findById(newWorkingProcess.getId()).get();
-//            if((newWorkingProcess.getIdPhongBan().getId()) == (oldWorkingProcess.getIdPhongBan().getId())){
-//                if(!repo.getByIdPhongBan(newWorkingProcess.getIdPhongBan().getId()).isEmpty()){
-//                    repo.save(newWorkingProcess);
-//                    return null;
-//                }
-//                else{
-//                    return "";
-//                }
-//            }
+        if (newWorkingProcess.getId() != null) {
+            List<WorkingProcess> wps = repo.getByEmpInRange(newWorkingProcess.getMaNV(), start, end);
+            if (!wps.isEmpty()) {
+               for(WorkingProcess w : wps){
+                   if(w.getId()!=newWorkingProcess.getId()){
+                       return "This employee had another working process from " + sdf.format(w.getNgayVao()) + " to " + sdf.format(w.getNgayRa()) + ". Working processes can't have dublicate dates";
+                   }
+               }
+            }
             repo.save(newWorkingProcess);
             return null;
         }
         // add
-        else{
-            DepartmentCategory departmentCategory = departmentCategoryRepository.findById(newWorkingProcess.getIdPhongBan().getId()).get();
-            if(departmentCategory.getId() != null){
-                repo.save(newWorkingProcess);
-                return null;
+        else {
+            WorkingProcess w = repo.getLastByEmp(newWorkingProcess.getMaNV());
+            if (w != null) {
+                if (w.getNgayRa() == null) {
+                    return "This employee is having an indefinite-term working process. Add failed";
+                }
             }
-            else{
-                return "";
+            List<WorkingProcess> wps = repo.getByEmpInRange(newWorkingProcess.getMaNV(), start, end);
+            if (!wps.isEmpty()) {
+                for(WorkingProcess wp : wps){
+                    if(wp.getId()!=newWorkingProcess.getId()){
+                        return "This employee had another working process from " + sdf.format(wp.getNgayVao()) + " to " + sdf.format(wp.getNgayRa()) + ". Working processes can't have dublicate dates";
+                    }
+                }
             }
+            repo.save(newWorkingProcess);
+            return null;
         }
     }
 
@@ -81,15 +111,26 @@ public class WorkingProcessService {
             s.setId(request.getId());
             s.setIdPhongBan(departmentCategoryRepository.findById(request.getIdPhongBan()).get());
             s.setIdChucVu(positionCategoryRepository.findById(request.getIdChucVu()).get());
-            if(request.getNgayVao()!=null)
-            s.setNgayVao(sdf.parse(request.getNgayVao()));
-            if(request.getNgayRa()!=null)
-            s.setNgayRa(sdf.parse(request.getNgayRa()));
+            if (request.getNgayVao() != null) {
+                s.setNgayVao(sdf.parse(request.getNgayVao()));
+            }
+            if (request.getNgayRa() != null && !request.getNgayRa().equals("")) {
+                s.setNgayRa(sdf.parse(request.getNgayRa()));
+            }
             s.setMaNV(request.getMaNV());
 
             return s;
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public int compareDate(Date d1, Date d2) {
+        Calendar c = Calendar.getInstance();
+        c.set(d1.getYear(), d1.getMonth(), d1.getDate());
+        d1 = c.getTime();
+        c.set(d2.getYear(), d2.getMonth(), d2.getDate());
+        d2 = c.getTime();
+        return d1.compareTo(d2);
     }
 }
